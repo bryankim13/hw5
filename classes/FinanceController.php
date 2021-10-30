48b78ff520c637e813a7e0313330e038cbe8c62d
Bryan Kim <bryankim13173@gmail.com>
2021-10-30 14:26:20 -0400

    organized some files

---
+++ classes/FinanceController.php
@@ -0,0 +1,73 @@
+<?php
+
+class TriviaController {
+
+    private $db;
+    
+    private $url = "";
+    
+    public function __construct() {
+        $this->db = new Database();
+    }
+    
+    public function run($command) {
+        
+        switch($command) {
+            case "question":
+                $this->question();
+                break;
+            case "logout":
+                $this->destroySession();
+            case "login":
+            default:
+                $this->login();
+                break;
+        }
+            
+    }
+    
+    private function destroySession() {          
+        session_destroy();
+
+        session_start();
+    }
+    
+    
+    public function login() {
+        // our login code from index.php last time!
+        $error_msg = "";
+        if (isset($_POST["email"])) { /// validate the email coming in
+            $data = $this->db->query("select * from user where email = ?;", "s", $_POST["email"]);
+            if ($data === false) {
+                $error_msg = "Error checking for user";
+            } else if (!empty($data)) { 
+                // user was found!
+                // validate the user's password
+                if (password_verify($_POST["password"], $data[0]["password"])) {
+                    $_SESSION["name"] = $data[0]["name"];
+                    $_SESSION["email"] = $data[0]["email"];
+                    $_SESSION["score"] = $data[0]["score"];
+                    header("Location: {$this->url}/question/");
+                    return;
+                } else {
+                    $error_msg = "Invalid Password";
+                }
+            } else {
+                $hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+                $insert = $this->db->query("insert into user (name, email, password) values (?, ?, ?);", "sss", $_POST["name"], $_POST["email"], $hash);
+                if ($insert === false) {
+                    $error_msg = "Error creating new user";
+                } 
+                
+                $_SESSION["name"] = $_POST["name"];
+                $_SESSION["email"] = $_POST["email"];
+                $_SESSION["score"] = 0;
+                header("Location: {$this->url}/question/");
+                return;
+            }
+
+        }
+
+        include "templates/login.php";
+    }
+}
---
+++ templates/transaction.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">  
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="your name">
+        <meta name="description" content="include some description about your page">  
+
+        <title>Trivia Game Login</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
+    </head>
+
+    <body>
+
+
+        <div class="container" style="margin-top: 15px;">
+            <div class="row col-xs-8">
+                <h1>CS4640 Television Trivia Game - Get Started</h1>
+                <p> Welcome to our trivia game!  To get started, login below or enter a new username and password to create an account</p>
+            </div>
+            <div class="row justify-content-center">
+                transactionsHistory
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </body>
+</html>
